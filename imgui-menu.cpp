714b00e0f0dfee145bf12e2904180fd66fa36d68
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2024-05-08 05:25:19 -0300

    init config

---
+++ imgui-menu.cpp
@@ -0,0 +1,12 @@
+﻿// imgui-menu.cpp : Defines the entry point for the application.
+//
+
+#include "imgui-menu.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
